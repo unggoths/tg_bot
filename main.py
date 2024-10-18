6cbd8dbfc3878afc9f488b661c687b77bc1d26ed
unggoths <160395525+unggoths@users.noreply.github.com>
2024-10-18 21:54:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from app.handlers import router, CallbackQuery
-
+from app.database.models import async_main
 async def main():
+    await async_main()
     bot = Bot(token='7545582976:AAFrEdvIOkIn_tzE36JAEeHkf_L0jN8_J5o')
     dp = Dispatcher()
 
