9334b06caedec323176d99a4ecb2d4cc6f6ba507
unggoths <160395525+unggoths@users.noreply.github.com>
2024-10-25 23:01:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+
 import asyncio
 from aiogram import Bot, Dispatcher
 
-from app.handlers import router, CallbackQuery
-from app.database.models import async_main
-async def main():
-    await async_main()
-    bot = Bot(token='7545582976:AAFrEdvIOkIn_tzE36JAEeHkf_L0jN8_J5o')
-    dp = Dispatcher()
+from app.handlers import router  # Правильний імпорт з app
+from database.models import create_connection, close_connection  # Змінено на правильний імпорт
 
-    dp.include_router(router)
-    await dp.start_polling(bot)
+async def main():
+    conn = create_connection()  # Викликаємо функцію для створення з'єднання
+    try:
+        bot = Bot(token='7545582976:AAFrEdvIOkIn_tzE36JAEeHkf_L0jN8_J5o')  # Замініть на ваш токен
+        dp = Dispatcher()
+
+        dp.include_router(router)
+        await dp.start_polling(bot)
+    finally:
+        close_connection(conn)  # Закриваємо з'єднання
 
 if __name__ == '__main__':
     try:
